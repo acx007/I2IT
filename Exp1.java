f4146bfccf0d39bebc3a1fc5ea8b30a4947e94f1
aditir7 <166302822+aditir7@users.noreply.github.com>
2025-05-04 14:29:41 +0530

    Update Exp1.java

--- Exp1.java
+++ Exp1.java
@@ -3,40 +3,61 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-public class Exp1 extends Frame implements KeyListener,ActionListener {
+
+public class KeyListenerEg extends Frame implements KeyListener 
+{
     Label l;
     TextArea area;
-    Exp1()
+
+    KeyListenerEg()
     {
-//Label Crean
+        // Label Creation
         l = new Label();
         l.setBounds(20, 50, 100, 20);
-// Text area creatn
+        
+        // Text area creation
         area = new TextArea();
-        area.setBounds(20,80,300,300);
+        area.setBounds(20, 80, 300, 300);
         area.addKeyListener(this);
+
         add(l);
         add(area);
-        setSize(400,400);
+
+        setSize(400, 400);
         setLayout(null);
         setVisible(true);
+
+        // Add window listener to close the window
+        addWindowListener(new java.awt.event.WindowAdapter() 
+        {
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) 
+            {
+                System.exit(0);
+            }
+        });
     }
-    public void keyTyped(KeyEvent var1) {
+
+    @Override
+    public void keyTyped(KeyEvent e) 
+    {
         l.setText("Key typed");
     }
+
     @Override
-    public void keyPressed(KeyEvent var1) {
+    public void keyPressed(KeyEvent e)
+    {
         l.setText("Key Pressed");
     }
+
     @Override
-    public void keyReleased(KeyEvent var1) {
+    public void keyReleased(KeyEvent e) 
+    {
         l.setText("Key Released");
     }
-    public static void main (String args[])
+
+    public static void main(String[] args) 
     {
-        new Exp1();
+        new KeyListenerEg();
     }
-    @Override
-    public void actionPerformed(ActionEvent var1) {
-    }
+    
 }
